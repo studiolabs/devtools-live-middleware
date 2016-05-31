4b93b9d248fe6b2ce21fe0ea278bf6f4444554d8
Monteiro Steed <steed.monteiro@gmail.com>
2016-05-31 14:21:58 +0200

    add file streaming

--- index.js
+++ index.js
@@ -73,9 +73,28 @@ module.exports = function(root, options) {
 		}
 
 
-		res.setHeader("Content-Length", content.length);
-		return res.end(content, 'binary');
+	var stat = fs.statSync(path);
+	  var total = stat.size;
 
+	  if (req.headers.range) {   // meaning client (browser) has moved the forward/back slider
+	                                         // which has sent this request back to this server logic ... cool
+	    var range = req.headers.range;
+	    var parts = range.replace(/bytes=/, "").split("-");
+	    var partialstart = parts[0];
+	    var partialend = parts[1];
+
+	    var start = parseInt(partialstart, 10);
+	    var end = partialend ? parseInt(partialend, 10) : total-1;
+	    var chunksize = (end-start)+1;
+
+	    var file = fs.createReadStream(path, {start: start, end: end});
+	    res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': contentType });
+	    file.pipe(res);
+
+	  } else {
+	  	res.setHeader("Content-Length", content.length);
+		res.end(content, 'binary');
+	  }
 	}
 
 	return devtoolsLiveMiddleware;
